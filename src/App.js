a69a1a337ceb404c9f1c2c26f1d063132b47e2f9
Yash Mukund Purohit <purohityash03@gmail.com>
2021-11-11 21:21:09 +0530

    all changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import {useDispatch } from 'react-redux'
+import Button from './Components/Button/Button';
+import Main from './Components/Main/Main';
 import './App.css';
+import {  datafetchingAction } from './ReduxStore/actions/datafetchingAction';
+import LoadMore from './Components/loadmore/LoadMore';
+
 
 function App() {
+  const dispatch = useDispatch() 
+  const [search, setsearch] = useState(""); 
+  const [text , setText] = useState("Random") ;
+  const [clicked, setClicked] = useState(false);
+  
+  const inputHandler =(e) =>{
+  
+      setsearch(e.target.value);
+  
+  }
+   const handlekeyPress=(e) =>{
+       if(e.key === "Enter"){
+         searchHandler();
+       }
+  }
+  const searchHandler = () =>{
+      
+      dispatch(datafetchingAction({search}));
+      if(search){
+        setText(search);
+      }
+    
+      setClicked(true);
+  }
+   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="app_header"> 
+           <input type="text" onKeyPress={handlekeyPress} placeholder="Search for photos" value={search} onChange ={inputHandler} />
+          
+           <Button logo={"search"} onClick={searchHandler}/>  
+        </div>
+        
+        <Main text={text} clicked={clicked} />
+        <LoadMore search={search} clicked={clicked}/>
+        
     </div>
   );
 }
